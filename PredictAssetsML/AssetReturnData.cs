d53242b6b2e32053c3f12cc61d0f8d1781c9c0b9
Gustavo Araujo <gustavobr1989@hotmail.com>
2024-10-13 19:29:21 -0300

    Cleaning code

--- PredictAssetsML/AssetReturnData.cs
+++ PredictAssetsML/AssetReturnData.cs
@@ -3,11 +3,11 @@
     public class AssetReturnData
     {
         public DateTime Date { get; set; } 
-        public float Price { get; set; }
+        //public float Price { get; set; }
         public float Yield { get; set; }
 
-        public float TD10 { get; set; } 
-        public float TD30 { get; set; } 
+        public float TD10 { get; set; }
+        public float TD30 { get; set; }
         public float TD90 { get; set; }
 
         public float TD10Y { get; set; }
--- PredictAssetsML/Program.cs
+++ PredictAssetsML/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.ML;
 using Microsoft.ML.Data;
-using System.Globalization;
 
 namespace PredictAssetsML
 {
@@ -13,139 +12,120 @@ namespace PredictAssetsML
     {
         static void Main(string[] args)
         {
-            // 1. Inicializa o contexto do ML
-            var mlContext = new MLContext();
+            //new Comparer().Compare();
 
-            // 2. Importa os dados de rentabilidade de ativos
-            var assetReturnsData = LoadAssetReturnsData("Data.csv");
-
-            // 3. Processa os dados para calcular rentabilidades acumuladas
-            var processedData = ProcessAssetReturns(assetReturnsData);
-
-            IDataView data = mlContext.Data.LoadFromEnumerable(processedData);
-
-            var trainTestData = mlContext.Data.TrainTestSplit(data, 0.2);
+            new Tester().Test();
 
             // 4. Treina o modelo
-            var model = TrainModel(mlContext, trainTestData.TrainSet);
+            //var model = TrainModel(mlContext, trainTestData.TrainSet);
+
+            //Func<EstimatorChain<Microsoft.ML.Transforms.NormalizingTransformer>, EstimatorChain<RegressionPredictionTransformer<FastForestRegressionModelParameters>>> generateFastForest = 
+            //    _ => _.Append(mlContext.Regression.Trainers.FastForest(labelColumnName: nameof(AssetReturnData.Yield), numberOfLeaves: 100, numberOfTrees: 1000));
+
+            //GetMetrics(mlContext, trainTestData, generateFastForest);
+
+            //Func<EstimatorChain<Microsoft.ML.Transforms.NormalizingTransformer>, EstimatorChain<RegressionPredictionTransformer<LinearRegressionModelParameters>>> generateSdca =
+            //    _ => _.Append(mlContext.Regression.Trainers.Sdca(labelColumnName: nameof(AssetReturnData.Yield)));
+
+            //GetMetrics(mlContext, trainTestData, generateSdca);
+
+            //Func<EstimatorChain<Microsoft.ML.Transforms.NormalizingTransformer>, EstimatorChain<RegressionPredictionTransformer<PoissonRegressionModelParameters>>> generateLasso =
+            //    _ => _.Append(mlContext.Regression.Trainers.LbfgsPoissonRegression(labelColumnName: nameof(AssetReturnData.Yield)));
+
+            //GetMetrics(mlContext, trainTestData, generateLasso);
+
+            //var predictedValues = mlContext.Data.CreateEnumerable<PredictionOutput>(predictions, reuseRowObject: false).ToList();
+
+            //// 6. Exibe as previsões
+            //foreach (var prediction in predictedValues)
+            //{
+            //    Console.WriteLine(prediction.Score);
+            //}
+
+            //var assetReturnsData = LoadAssetReturnsData("Data.csv");
+
+            //var mlContext = new MLContext();
+
+            //// 2. Importa os dados de rentabilidade de ativos
+
+            //// 3. Processa os dados para calcular rentabilidades acumuladas
+            //var processedData = ProcessAssetReturns(assetReturnsData);
+
+            //IDataView data = mlContext.Data.LoadFromEnumerable(processedData);
+
+            //var trainTestData = mlContext.Data.TrainTestSplit(data, 0.2);
+            //Func<EstimatorChain<Microsoft.ML.Transforms.NormalizingTransformer>, EstimatorChain<RegressionPredictionTransformer<FastTreeRegressionModelParameters>>> generateFastTree =
+            //_ => _.Append(mlContext.Regression.Trainers.FastTree(labelColumnName: nameof(AssetReturnData.Yield), numberOfLeaves: 100, numberOfTrees: 1000));
+
+            //Func<EstimatorChain<Microsoft.ML.Transforms.NormalizingTransformer>, EstimatorChain<RegressionPredictionTransformer<LinearRegressionModelParameters>>> generateSdca =
+            //    _ => _.Append(mlContext.Regression.Trainers.Sdca(labelColumnName: nameof(AssetReturnData.Yield)));
+
+            //for (var i = 0; i < 30; i++) 
+            //{
+            //    var recentData = processedData.Skip(Math.Max(0, processedData.Count() - 30)).Take(30).ToList();
+
+            //    var lastAssetReturnData = new AssetReturnData 
+            //    {
+            //        Yield = recentData.Average(_ => _.Yield),
+            //        TD10Y = recentData.Average(_ => _.TD10Y),
+            //        TD30Y = recentData.Average(_ => _.TD30Y),
+            //        TD90Y = recentData.Average(_ => _.TD90Y),
+            //    };
+
+            //    //lastAssetReturnData.Yield = 0;
+            //    var newYield = GetPredictedValue(mlContext, trainTestData, generateFastTree, lastAssetReturnData);
+            //    Console.WriteLine(newYield);
+
+            //    processedData.Add(new AssetReturnData { Date = lastAssetReturnData.Date.AddDays(1), Yield = newYield });
+            //    processedData = ProcessAssetReturns(processedData);
+            //}
+        }
+
+        private static float GetPredictedValue<T>(MLContext mlContext, DataOperationsCatalog.TrainTestData trainTestData, 
+            Func<EstimatorChain<Microsoft.ML.Transforms.NormalizingTransformer>, EstimatorChain<RegressionPredictionTransformer<T>>> createPipeline, AssetReturnData lastAssetReturnData) where T : class
+        {
+            var estimator = Comparer.CreateEstimator(mlContext);
+                //.Append(mlContext.Regression.Trainers.Sdca(labelColumnName: nameof(AssetReturnData.IBOV), maximumNumberOfIterations: 100));
+                
+            //var pipeline = estimator.Append(mlContext.Regression.Trainers.FastForest(labelColumnName: nameof(AssetReturnData.Yield), numberOfLeaves: 100, numberOfTrees: 1000));
+
+            var pipeline = createPipeline.Invoke(estimator);
+
+            var model = pipeline.Fit(trainTestData.TrainSet);
 
             var predictions = model.Transform(trainTestData.TestSet);
-            var metrics = mlContext.Regression.Evaluate(predictions, labelColumnName: nameof(AssetReturnData.Price));
-
+            //var metrics = mlContext.Regression.Evaluate(predictions, labelColumnName: nameof(AssetReturnData.Yield));
 
             var predictedValues = mlContext.Data.CreateEnumerable<PredictionOutput>(predictions, reuseRowObject: false).ToList();
-
-            // 6. Exibe as previsões
+            float lastPrediction = 0;
             foreach (var prediction in predictedValues)
             {
+                if (float.IsNaN(prediction.Score))
+                {
+                    prediction.Score = lastPrediction;
+                }
+
+                lastPrediction = prediction.Score;
                 Console.WriteLine(prediction.Score);
             }
-        }
 
-        // Método para carregar dados de um arquivo CSV
-        private static List<AssetReturnData> LoadAssetReturnsData(string filePath)
-        {
-            var lines = File.ReadAllLines(filePath);
-            var data = new List<AssetReturnData>();
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<AssetReturnData, AssetReturnPrediction>(model);
 
-            // Lógica para carregar e converter os dados do CSV
-            foreach (var line in lines.Skip(1)) // Ignora o cabeçalho
-            {
-                var values = line.Split(',');
-                var assetReturnData = new AssetReturnData
-                {
-                    Date = DateTime.Parse(values[0]),
-                    Price = float.Parse(values[1], CultureInfo.InvariantCulture),
-                    Yield = float.Parse(values[2], CultureInfo.InvariantCulture),
-                };
-                data.Add(assetReturnData);
-            }
+            AssetReturnPrediction result = new();
+            predictionEngine.Predict(lastAssetReturnData, ref result);
 
-            return data;
-        }
+            return result.Score;
 
-        // Método para processar as rentabilidades e calcular acumulados
-        private static List<AssetReturnData> ProcessAssetReturns(List<AssetReturnData> assetReturnsData)
-        {
-            // Aqui você irá calcular as rentabilidades acumuladas para os períodos de 6, 12 e 18 meses
-            // Similar ao que discutimos anteriormente
-            // Retorna a lista processada
-            var processedData = new List<AssetReturnData>();
-
-            for (var i = 10; i < assetReturnsData.Count; i++)
-            {
-                if (assetReturnsData[i].Price == 0 || assetReturnsData[i].Yield == 0) continue;
-
-                if (i > 10)
-                {
-                    assetReturnsData[i].TD10 = assetReturnsData[i].Price/ assetReturnsData[i-10].Price;
-                    assetReturnsData[i].TD10Y = assetReturnsData[i].Yield/ assetReturnsData[i-10].Yield;
-                }
-
-                if (i > 30)
-                {
-                    assetReturnsData[i].TD30 = assetReturnsData[i].Price / assetReturnsData[i - 30].Price;
-                    assetReturnsData[i].TD30Y = assetReturnsData[i].Yield / assetReturnsData[i - 30].Yield;
-                }
-
-                if (i > 90)
-                {
-                    assetReturnsData[i].TD90 = assetReturnsData[i].Price / assetReturnsData[i - 90].Price;
-                    assetReturnsData[i].TD90Y = assetReturnsData[i].Yield / assetReturnsData[i - 90].Yield;
-                }
-
-                processedData.Add(assetReturnsData[i]);
-            }
-
-            return processedData;
-        }
-
-        // Método para treinar o modelo
-        private static ITransformer TrainModel(MLContext mlContext, IDataView trainData)
-        {
-            var pipeline = mlContext.Transforms.Concatenate("Features",
-                nameof(AssetReturnData.TD30),
-                nameof(AssetReturnData.TD90),
-                nameof(AssetReturnData.TD10),
-                nameof(AssetReturnData.TD30Y),
-                nameof(AssetReturnData.TD90Y),
-                nameof(AssetReturnData.TD10Y),
-                nameof(AssetReturnData.Price),
-                nameof(AssetReturnData.Yield)
-                )
-                .Append(mlContext.Transforms.NormalizeMinMax("Features"))
-                //.Append(mlContext.Regression.Trainers.Sdca(labelColumnName: nameof(AssetReturnData.IBOV), maximumNumberOfIterations: 100));
-                .Append(mlContext.Regression.Trainers.FastForest(labelColumnName: nameof(AssetReturnData.Price),numberOfLeaves:100,numberOfTrees:1000));
-
-            return pipeline.Fit(trainData);
-        }
-
-        public class PredictionResult
-        {
-            [ColumnName("Score")]
-            public float PredictedReturn { get; set; } // A previsão de rentabilidade do IBOV
-        }
-
-        // Método para prever as rentabilidades futuras
-        private static float PredictFutureReturn(MLContext mlContext, ITransformer model, AssetReturnData currentData)
-        {
-            var predictionEngine = mlContext.Model.CreatePredictionEngine<AssetReturnData, PredictionResult>(model);
-            var prediction = predictionEngine.Predict(currentData);
-            return prediction.PredictedReturn;
-        }
-
-        // Adicione aqui a função UpdateAssetReturnDataWithPrediction
-        private static AssetReturnData UpdateAssetReturnDataWithPrediction(AssetReturnData previousData, float predictedReturn)
-        {
-            // Implementação para atualizar as rentabilidades
-            return previousData; // Retorna os dados atualizados
+            //Console.WriteLine(nameof(T) + ":");
+            //Console.WriteLine("RSquared: " + metrics.RSquared);
+            //Console.WriteLine("MeanSquaredError: " + metrics.MeanSquaredError);
+            //Console.WriteLine("RootMeanSquaredError: " + metrics.RootMeanSquaredError);
+            //Console.WriteLine("MeanAbsoluteError: " + metrics.MeanAbsoluteError);
         }
     }
 
-    public class PredictionResult
+    public class AssetReturnPrediction
     {
-        [ColumnName("Score")]
-        public float PredictedReturn { get; set; } // A previsão de rentabilidade do IBOV
+        public float Score { get; set; }  // O valor previsto (Yield) será armazenado aqui.
     }
-
 }
